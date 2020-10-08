8c30c86e5122b16122bfe400a2b93de5e5a495bf
jpapejr <jpapejr@icloud.com>
2020-10-08 12:52:25 -0400

    removing unneeded vars

--- variables.tf
+++ variables.tf
@@ -1,19 +1,8 @@
 variable "user" {
 }
 
-variable "org" {
-}
-
-variable "space" {
-}
-
-variable "region" {
-}
 
 variable "resource_group" {
-}
-
-variable "resource_instance" {
 }
 
 variable "cluster" { 
